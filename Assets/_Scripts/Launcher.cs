8de42adc914fee7e25788ad1865d5e0785c8dcdb
Lara-Tiara <charlotte1048820903@gmail.com>
2024-04-18 01:08:51 +0100

    RoomName and NickName Valid Check

--- Assets/_Scripts/Launcher.cs
+++ Assets/_Scripts/Launcher.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using Photon.Realtime;
 using TMPro;
+using System.Text.RegularExpressions;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
@@ -20,6 +21,7 @@ public class Launcher : MonoBehaviourPunCallbacks
     public TMP_InputField roomName;
     public TMP_InputField playerName;
     public int playerCount;
+    public GameObject inValidInputText;
 
     void Start()
     {
@@ -41,22 +43,52 @@ public class Launcher : MonoBehaviourPunCallbacks
 
     public void StartButton()
     {
+        string nickname = nickNameIpt.GetComponent<TMP_InputField>().text;
+
+        if (string.IsNullOrWhiteSpace(nickname) || !Regex.IsMatch(nickname, @"^[a-zA-Z0-9]+$"))
+        {
+            inValidInputText.GetComponent<TextMeshProUGUI>().text = "Nickname cannot be empty and must contain only letters and numbers.";
+            StartCoroutine(InValidInputText());
+            //Debug.LogError("Nickname cannot be empty and must contain only letters and numbers.");
+            nameUI.SetActive(true);
+            return;
+        }
+
         nameUI.SetActive(false);
-        PhotonNetwork.NickName = nickNameIpt.GetComponent<TMP_InputField>().text;
+        PhotonNetwork.NickName = nickname;
+        Debug.Log("User Nickname: " + PhotonNetwork.LocalPlayer.NickName);
+        Debug.Log("User ID: " + PhotonNetwork.LocalPlayer.UserId);
         loginUI.SetActive(true);
-        if(PhotonNetwork.InLobby)
+        if (PhotonNetwork.InLobby)
         {
             roomListUI.SetActive(true);
         }
     }
+
+    private IEnumerator InValidInputText()
+    {
+        inValidInputText.SetActive(true);
+        yield return new WaitForSeconds(3);
+        inValidInputText.SetActive(false);
+    }
     
     public void JoinGame()
     {
-        if(!PhotonNetwork.IsConnected)
+        if (!PhotonNetwork.IsConnected)
+        {
+            Debug.LogError("Not connected to Photon.");
             return;
+        }
 
-        if(roomName.text.Length < 2)
+        string room = roomName.text;
+        if (string.IsNullOrWhiteSpace(room) || room.Length < 3 || !Regex.IsMatch(room, @"^[a-zA-Z0-9]+$"))
+        {
+            inValidInputText.GetComponent<TextMeshProUGUI>().text = "Room name cannot be empty, must be at least three characters long, and contain only letters and numbers.";
+            StartCoroutine(InValidInputText());
+            //Debug.LogError("Room name cannot be empty, must be at least three characters long, and contain only letters and numbers.");
+            loginUI.SetActive(true);
             return;
+        }
 
         loginUI.SetActive(false);
         PhotonNetwork.JoinOrCreateRoom(roomName.text, new Photon.Realtime.RoomOptions { MaxPlayers = playerCount }, default);
